c995e24718a576b76091986fdd4860dbfa7be4fc
tom <tom@yubico.com>
2014-03-07 13:48:00 +0100

    fixed patch.sh

--- yubicoauthenticator/resources/osx/patch.sh
+++ yubicoauthenticator/resources/osx/patch.sh
@@ -1,7 +1,7 @@
 #!/bin/bash          
 echo Apple does not support smart cards. Please contact Apple and ask for smartcard support. 
 echo Meanwhile Yubico will fix this for you...
-wait 3 
+sleep 1
 echo Applying patch...
-#patch -p0 libccid-yubikey.diff
+patch -p0 < libccid-yubikey.diff
 echo ...done!
