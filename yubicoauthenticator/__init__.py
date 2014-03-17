e72cd39c8692dcca594af258e2680f8bc21cc076
Tom <tom@yubico.com>
2014-03-17 11:23:22 +0100

    news

--- yubicoauthenticator/__init__.py
+++ yubicoauthenticator/__init__.py
@@ -13,4 +13,4 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-__version__ = "0.0.5"
+__version__ = "0.0.6"
