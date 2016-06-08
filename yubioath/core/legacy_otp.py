1742d46d5c7ed8f59e91764ac30bd115e9467774
Dain Nilsson <dain@yubico.com>
2016-06-08 22:41:10 +0200

    Update yubicommon, switch to use_library.

--- yubioath/core/legacy_otp.py
+++ yubioath/core/legacy_otp.py
@@ -29,29 +29,14 @@ from __future__ import print_function
 from .utils import time_challenge, parse_full, format_code
 from .standard import TYPE_TOTP
 from .exc import InvalidSlotError, NeedsTouchError
-from yubioath.yubicommon.ctypes import load_library
+from yubioath.yubicommon.ctypes import use_library
 from hashlib import sha1
 from ctypes import (Structure, POINTER, c_int, c_uint8, c_uint, c_char_p,
                     c_bool, sizeof, create_string_buffer, cast, addressof)
 import weakref
 
-_lib = load_library('ykpers-1', '1')
-if not hasattr(_lib, 'yk_init'):
-    raise ImportError('Can\'t find ykpers library')
 
-
-def define(name, args, res):
-    try:
-        fn = getattr(_lib, name)
-        fn.argtypes = args
-        fn.restype = res
-    except AttributeError:
-        print("Undefined symbol: %s" % name)
-
-        def error(*args, **kwargs):
-            raise Exception("Undefined symbol: %s" % name)
-        fn = error
-    return fn
+ykpers = use_library('ykpers-1', '1')
 
 
 SLOTS = [
@@ -62,15 +47,16 @@ SLOTS = [
 
 YK_KEY = type('YK_KEY', (Structure,), {})
 
-_yk_errno_location = define('_yk_errno_location', [], POINTER(c_int))
-yk_init = define('yk_init', [], bool)
-yk_release = define('yk_release', [], bool)
-ykpers_check_version = define('ykpers_check_version', [c_char_p], c_char_p)
 
-yk_open_first_key = define('yk_open_first_key', [], POINTER(YK_KEY))
-yk_close_key = define('yk_close_key', [POINTER(YK_KEY)], bool)
+_yk_errno_location = ykpers('_yk_errno_location', [], POINTER(c_int))
+yk_init = ykpers('yk_init', [], bool)
+yk_release = ykpers('yk_release', [], bool)
+ykpers_check_version = ykpers('ykpers_check_version', [c_char_p], c_char_p)
 
-yk_challenge_response = define('yk_challenge_response',
+yk_open_first_key = ykpers('yk_open_first_key', [], POINTER(YK_KEY))
+yk_close_key = ykpers('yk_close_key', [POINTER(YK_KEY)], bool)
+
+yk_challenge_response = ykpers('yk_challenge_response',
                                [POINTER(YK_KEY), c_uint8, c_int, c_uint,
                                 c_char_p, c_uint, c_char_p], bool)
 
@@ -84,36 +70,36 @@ YKP_CONFIG = type('YKP_CONFIG', (Structure,), {})
 YK_CONFIG = type('YK_CONFIG', (Structure,), {})
 YK_STATUS = type('YK_STATUS', (Structure,), {})
 
-ykds_alloc = define('ykds_alloc', [], POINTER(YK_STATUS))
-ykds_free = define('ykds_free', [POINTER(YK_STATUS)], None)
-ykds_touch_level = define('ykds_touch_level', [POINTER(YK_STATUS)], c_int)
-yk_get_status = define('yk_get_status', [
+ykds_alloc = ykpers('ykds_alloc', [], POINTER(YK_STATUS))
+ykds_free = ykpers('ykds_free', [POINTER(YK_STATUS)], None)
+ykds_touch_level = ykpers('ykds_touch_level', [POINTER(YK_STATUS)], c_int)
+yk_get_status = ykpers('yk_get_status', [
     POINTER(YK_KEY), POINTER(YK_STATUS)], c_int)
 
-ykp_alloc = define('ykp_alloc', [], POINTER(YKP_CONFIG))
-ykp_free_config = define('ykp_free_config', [POINTER(YKP_CONFIG)], bool)
+ykp_alloc = ykpers('ykp_alloc', [], POINTER(YKP_CONFIG))
+ykp_free_config = ykpers('ykp_free_config', [POINTER(YKP_CONFIG)], bool)
 
-ykp_configure_version = define('ykp_configure_version',
+ykp_configure_version = ykpers('ykp_configure_version',
                                [POINTER(YKP_CONFIG), POINTER(YK_STATUS)], None)
 
-ykp_HMAC_key_from_raw = define('ykp_HMAC_key_from_raw',
+ykp_HMAC_key_from_raw = ykpers('ykp_HMAC_key_from_raw',
                                [POINTER(YKP_CONFIG), c_char_p], bool)
-ykp_set_tktflag_CHAL_RESP = define('ykp_set_tktflag_CHAL_RESP',
+ykp_set_tktflag_CHAL_RESP = ykpers('ykp_set_tktflag_CHAL_RESP',
                                    [POINTER(YKP_CONFIG), c_bool], bool)
-ykp_set_cfgflag_CHAL_HMAC = define('ykp_set_cfgflag_CHAL_HMAC',
+ykp_set_cfgflag_CHAL_HMAC = ykpers('ykp_set_cfgflag_CHAL_HMAC',
                                    [POINTER(YKP_CONFIG), c_bool], bool)
-ykp_set_cfgflag_HMAC_LT64 = define('ykp_set_cfgflag_HMAC_LT64',
+ykp_set_cfgflag_HMAC_LT64 = ykpers('ykp_set_cfgflag_HMAC_LT64',
                                    [POINTER(YKP_CONFIG), c_bool], bool)
-ykp_set_extflag_SERIAL_API_VISIBLE = define(
+ykp_set_extflag_SERIAL_API_VISIBLE = ykpers(
     'ykp_set_extflag_SERIAL_API_VISIBLE', [POINTER(YKP_CONFIG), c_bool], bool)
-ykp_set_extflag_ALLOW_UPDATE = define('ykp_set_extflag_ALLOW_UPDATE',
+ykp_set_extflag_ALLOW_UPDATE = ykpers('ykp_set_extflag_ALLOW_UPDATE',
                                       [POINTER(YKP_CONFIG), c_bool], bool)
-ykp_set_cfgflag_CHAL_BTN_TRIG = define('ykp_set_cfgflag_CHAL_BTN_TRIG',
+ykp_set_cfgflag_CHAL_BTN_TRIG = ykpers('ykp_set_cfgflag_CHAL_BTN_TRIG',
                                        [POINTER(YKP_CONFIG), c_bool], bool)
 
-ykp_core_config = define('ykp_core_config', [POINTER(YKP_CONFIG)],
+ykp_core_config = ykpers('ykp_core_config', [POINTER(YKP_CONFIG)],
                          POINTER(YK_CONFIG))
-yk_write_command = define('yk_write_command',
+yk_write_command = ykpers('yk_write_command',
                           [POINTER(YK_KEY), POINTER(YK_CONFIG), c_uint8,
                            c_char_p], bool)
 
@@ -128,16 +114,6 @@ ykpers_version = ykpers_check_version(None).decode('ascii')
 
 def yk_get_errno():
     return _yk_errno_location().contents.value
-
-
-class YkWrapper(object):
-
-    def __init__(self, key):
-        self.key = key
-
-    def __del__(self):
-        yk_close_key(self.key)
-        del self.key
 
 
 class LegacyOathOtp(object):
