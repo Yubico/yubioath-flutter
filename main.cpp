7f43dedd72c33337ab3e9dbf3d1b266712bf462c
Dag Heyman <dag@yubico.com>
2017-08-08 12:47:09 +0200

    Use ANGLE on windows, but use an attribute.

--- main.cpp
+++ main.cpp
@@ -20,11 +20,6 @@ int main(int argc, char *argv[])
     // Don't write .pyc files.
     qputenv("PYTHONDONTWRITEBYTECODE", "1");
 
-    // Use ANGLE on WIndows
-    #ifdef Q_OS_WIN
-    qputenv("QT_OPENGL", "angle");
-    #endif
-
     #if QT_VERSION >= QT_VERSION_CHECK(5, 6, 0)
     QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
     #endif
@@ -47,6 +42,9 @@ int main(int argc, char *argv[])
     app.setApplicationName("Yubico Authenticator");
     app.setOrganizationName("Yubico");
     app.setOrganizationDomain("com.yubico");
+
+    // Use ANGLE on Windows
+    app.setAttribute(Qt::AA_UseOpenGLES);
 
     if (QFileInfo::exists(":" + main_qml)) {
         // Embedded resources
