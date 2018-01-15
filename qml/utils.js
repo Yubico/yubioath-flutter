3f679d4f69357d26aedb9da288a9325734b8f90b
Emil Lundberg <emil@yubico.com>
2018-01-15 10:12:50 +0100

    Merge Util.qml into utils.js

--- qml/utils.js
+++ qml/utils.js
@@ -8,3 +8,41 @@ function versionGE(version, major, minor, micro) {
   }
   return false
 }
+
+/*
+ * Quick and dirty shim for Array.find added in Qt 5.9
+ *
+ * @param items: array
+ * @param predicate: function(any) => Boolean
+ * @return The first `item` in `arr` that matches the `predicate`, or
+ *          `undefined` if none matches
+ */
+function find(items, predicate) {
+    if (items) {
+        for (var i = 0; i < items.length; ++i) {
+            if (predicate(items[i])) {
+                return items[i]
+            }
+        }
+    }
+    return undefined
+}
+
+/*
+ * Quick and dirty shim for Array.findIndex added in Qt 5.9
+ *
+ * @param items: array
+ * @param predicate: function(any) => Boolean
+ * @return The index of the first `item` in `arr` that matches the
+ *          `predicate`, or `undefined` if none matches
+ */
+function findIndex(items, predicate) {
+    if (items) {
+        for (var i = 0; i < items.length; ++i) {
+            if (predicate(items[i])) {
+                return i
+            }
+        }
+    }
+    return undefined
+}
