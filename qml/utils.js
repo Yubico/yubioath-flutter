f39d0d3fb715a261fceba837416a7da40d6e7b6a
Dag Heyman <dag@yubico.com>
2018-01-15 10:42:58 +0100

    Autoformat utils.js

--- qml/utils.js
+++ qml/utils.js
@@ -3,10 +3,13 @@ function versionString(version) {
 }
 
 function versionGE(version, major, minor, micro) {
-  if(version != undefined) {
-    return (version[0] > major || (version[0] == major && (version[1] > minor || (version[1] == minor && version[2] >= micro))))
-  }
-  return false
+    if (version != undefined) {
+        return (version[0] > major || (version[0] == major
+                                       && (version[1] > minor
+                                           || (version[1] == minor
+                                               && version[2] >= micro))))
+    }
+    return false
 }
 
 /*
