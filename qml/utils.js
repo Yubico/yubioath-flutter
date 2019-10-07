62af4009a50db29a0390c08ee08f71f8078824a1
Dag Heyman <dag@yubico.com>
2019-10-07 12:47:45 +0200

    utils: add reference for hash code implementation

--- qml/utils.js
+++ qml/utils.js
@@ -7,6 +7,7 @@ function getNow() {
 /*
  * Returns a hash code for a string.
  * (Compatible to Java's String.hashCode())
+ *  See: https://gist.github.com/hyamamoto/fd435505d29ebfa3d9716fd2be8d42f0
  */
 function hashCode(s) {
   var h = 0, l = s.length, i = 0;
