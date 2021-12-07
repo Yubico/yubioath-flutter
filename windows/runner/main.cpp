f90f513ca07718c9e9cd67a2166c754238432f95
Dain Nilsson <dain@yubico.com>
2021-12-07 12:41:32 +0100

    Set initial window size.

--- windows/runner/main.cpp
+++ windows/runner/main.cpp
@@ -46,7 +46,7 @@ int APIENTRY wWinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE prev,
 
   FlutterWindow window(project);
   Win32Window::Point origin(10, 10);
-  Win32Window::Size size(1280, 720);
+  Win32Window::Size size(400, 720);
   if (!window.CreateAndShow(L"Yubico Authenticator", origin, size)) {
     return EXIT_FAILURE;
   }
