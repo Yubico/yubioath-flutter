8253c11dbc62d7afd33db7d457442045f96607f6
Dain Nilsson <dain@yubico.com>
2022-06-07 13:04:05 +0200

    Ensure pip is updated for venv to generate licenses.

--- build-helper.bat
+++ build-helper.bat
@@ -10,6 +10,7 @@ echo Generating license files...
 rmdir /s /q ..\build\windows\helper-license-venv
 poetry build
 poetry run python -m venv ..\build\windows\helper-license-venv
+..\build\windows\helper-license-venv\Scripts\pip install --upgrade pip wheel
 ..\build\windows\helper-license-venv\Scripts\pip install dist\authenticator_helper-0.1.0-py3-none-any.whl pip-licenses
 ..\build\windows\helper-license-venv\Scripts\pip-licenses --format=json --no-license-path --with-license-file --ignore-packages authenticator-helper --output-file ..\assets\licenses\helper.json
 
