70d4bb8510da1992d410fcfd34b366ab65eb23d5
Dain Nilsson <dain@yubico.com>
2022-09-02 11:17:36 +0200

    Extract method for readability.

--- lib/widgets/choice_filter_chip.dart
+++ lib/widgets/choice_filter_chip.dart
@@ -28,6 +28,38 @@ class ChoiceFilterChip<T> extends StatefulWidget {
 class _ChoiceFilterChipState<T> extends State<ChoiceFilterChip<T>> {
   bool _showing = false;
 
+  Future<T?> _showPickerMenu() async {
+    final RenderBox chipBox = context.findRenderObject()! as RenderBox;
+    final RenderBox overlay =
+        Navigator.of(context).overlay!.context.findRenderObject()! as RenderBox;
+    final RelativeRect position = RelativeRect.fromRect(
+      Rect.fromPoints(
+        chipBox.localToGlobal(chipBox.size.bottomLeft(Offset.zero),
+            ancestor: overlay),
+        chipBox.localToGlobal(chipBox.size.bottomRight(Offset.zero),
+            ancestor: overlay),
+      ),
+      Offset.zero & overlay.size,
+    );
+
+    return await showMenu(
+      context: context,
+      position: position,
+      shape: const RoundedRectangleBorder(
+        borderRadius: BorderRadius.all(Radius.circular(4)),
+      ),
+      color: Theme.of(context).colorScheme.background,
+      items: widget.items
+          .map((e) => PopupMenuItem<T>(
+                value: e,
+                height: chipBox.size.height,
+                textStyle: Theme.of(context).chipTheme.labelStyle,
+                child: widget.itemBuilder(e),
+              ))
+          .toList(),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return FilterChip(
@@ -50,42 +82,11 @@ class _ChoiceFilterChipState<T> extends State<ChoiceFilterChip<T>> {
       showCheckmark: false,
       onSelected: widget.onChanged != null
           ? (_) async {
-              final RenderBox chipBox =
-                  context.findRenderObject()! as RenderBox;
-              final RenderBox overlay = Navigator.of(context)
-                  .overlay!
-                  .context
-                  .findRenderObject()! as RenderBox;
-              final RelativeRect position = RelativeRect.fromRect(
-                Rect.fromPoints(
-                  chipBox.localToGlobal(chipBox.size.bottomLeft(Offset.zero),
-                      ancestor: overlay),
-                  chipBox.localToGlobal(chipBox.size.bottomRight(Offset.zero),
-                      ancestor: overlay),
-                ),
-                Offset.zero & overlay.size,
-              );
-
               setState(() {
                 _showing = true;
               });
               try {
-                final selected = await showMenu(
-                  context: context,
-                  position: position,
-                  shape: const RoundedRectangleBorder(
-                    borderRadius: BorderRadius.all(Radius.circular(4)),
-                  ),
-                  color: Theme.of(context).colorScheme.background,
-                  items: widget.items
-                      .map((e) => PopupMenuItem<T>(
-                            value: e,
-                            height: chipBox.size.height,
-                            textStyle: Theme.of(context).chipTheme.labelStyle,
-                            child: widget.itemBuilder(e),
-                          ))
-                      .toList(),
-                );
+                final selected = await _showPickerMenu();
                 if (selected != null) {
                   widget.onChanged?.call(selected);
                 }
