066e4a32526061eff32e5b64b72bcdf80f27c3ff
Adam Velebil <adam.velebil@yubico.com>
2022-09-08 14:48:52 +0200

    remove unused import

--- lib/android/views/beta_dialog.dart
+++ lib/android/views/beta_dialog.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:url_launcher/url_launcher.dart';
 
 import '../../app/message.dart';
 import '../../core/state.dart';
