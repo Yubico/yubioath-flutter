e38767bfe4eb7cd89c45f2445eb84fb5ad79a26b
Adam Velebil <adam.velebil@yubico.com>
2022-10-04 15:47:26 +0200

    update kotlin sources

--- android/app/src/main/kotlin/com/yubico/authenticator/ndef/KeyboardLayout.kt
+++ android/app/src/main/kotlin/com/yubico/authenticator/ndef/KeyboardLayout.kt
@@ -1,22 +1,35 @@
+/*
+ * Copyright (C) 2022 Yubico.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.yubico.authenticator.ndef
 
 import java.lang.IllegalArgumentException
 
 abstract class KeyboardLayout(val name:String) {
     companion object {
-        const val DEFAULT_NAME = "US"
-
         const val SHIFT = 0x80
 
         private val layouts by lazy {
             listOf(
-                    USKeyboardLayout,
-                    DEKeyboardLayout,
-                    DECHKeyboardLayout
-            ).map { it.name to it }.toMap()
+                USKeyboardLayout,
+                DEKeyboardLayout,
+                DECHKeyboardLayout
+            ).associateBy { it.name }
         }
 
-        val availableLayouts get() = layouts.keys.toTypedArray()
         fun forName(name:String) = layouts[name] ?: throw IllegalArgumentException("Unsupported keyboard layout: $name!")
     }
 
@@ -29,12 +42,12 @@ abstract class KeyboardLayout(val name:String) {
 
 open class BaseKeyboardLayout(name:String, private val unshiftedMap: Array<String>, private val shiftedMap: Array<String>) : KeyboardLayout(name) {
     override fun fromScanCode(code: Int): String {
-        if (code < KeyboardLayout.SHIFT) {
+        if (code < SHIFT) {
             if (code < unshiftedMap.size) {
                 return unshiftedMap[code]
             }
         } else {
-            val shiftCode = code xor KeyboardLayout.SHIFT
+            val shiftCode = code xor SHIFT
             if (shiftCode < shiftedMap.size) {
                 return shiftedMap[shiftCode]
             }
