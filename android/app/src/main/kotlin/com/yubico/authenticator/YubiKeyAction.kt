dbecb1f581bd7677e7da420551c75e9e9640102c
Adam Velebil <adam.velebil@yubico.com>
2022-10-05 12:42:24 +0200

    delete unused class

--- android/app/src/main/kotlin/com/yubico/authenticator/YubiKeyAction.kt
+++
@@ -1,9 +0,0 @@
-package com.yubico.authenticator
-
-import com.yubico.yubikit.core.YubiKeyDevice
-import com.yubico.yubikit.core.util.Result
-
-data class YubiKeyAction(
-    val message: String,
-    val action: suspend (Result<YubiKeyDevice, Exception>) -> Unit
-)
