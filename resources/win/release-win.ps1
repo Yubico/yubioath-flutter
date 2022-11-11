79ceeffdc49b28f580c7b0d710ef98991793050f
Dennis Fokin <Dennis.Fokin@yubico.com>
2022-11-11 11:22:34 +0100

    windows release fixes

--- resources/win/release-win.ps1
+++ resources/win/release-win.ps1
@@ -10,7 +10,7 @@ signtool.exe sign /fd SHA256 /t http://timestamp.digicert.com/scripts/timstamp.d
 echo "Setting env var and building installer"
 $env:SRCDIR = ".\release\"
 heat dir .\release -out fragment.wxs -gg -scom -srd -sfrag -dr INSTALLDIR -cg ApplicationFiles -var env.SRCDIR
-candle .\fragment.wxs .\resources\win\yubioath-desktop.wxs -ext WixUtilExtension -arch x64
+candle .\fragment.wxs yubioath-desktop.wxs -ext WixUtilExtension -arch x64
 light fragment.wixobj yubioath-desktop.wixobj -ext WixUIExtension -ext WixUtilExtension -o yubioath-desktop-$version-win64.msi
 
 echo "Signing the installer"
