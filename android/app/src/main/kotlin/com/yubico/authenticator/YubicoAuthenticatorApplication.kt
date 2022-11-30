b7bb2f0cc021589cb3ef16924b12d11dac3be820
Adam Velebil <adam.velebil@yubico.com>
2022-11-30 09:57:05 +0100

    add hilt dependencies

---
+++ android/app/src/main/kotlin/com/yubico/authenticator/YubicoAuthenticatorApplication.kt
@@ -0,0 +1,7 @@
+package com.yubico.authenticator
+
+import dagger.hilt.android.HiltAndroidApp
+import io.flutter.app.FlutterApplication
+
+@HiltAndroidApp
+class YubicoAuthenticatorApplication : FlutterApplication()
