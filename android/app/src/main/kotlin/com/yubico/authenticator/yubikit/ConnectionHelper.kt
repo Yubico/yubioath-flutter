0c1a844df07bdcf0acf617bfbff52879771014f0
Adam Velebil <adam.velebil@yubico.com>
2023-01-17 09:33:15 +0100

    handle exceptions from runCatching

--- android/app/src/main/kotlin/com/yubico/authenticator/yubikit/ConnectionHelper.kt
+++ android/app/src/main/kotlin/com/yubico/authenticator/yubikit/ConnectionHelper.kt
@@ -18,14 +18,16 @@ package com.yubico.authenticator.yubikit
 
 import com.yubico.yubikit.core.YubiKeyConnection
 import com.yubico.yubikit.core.YubiKeyDevice
+import kotlin.coroutines.resume
+import kotlin.coroutines.resumeWithException
 import kotlin.coroutines.suspendCoroutine
 
 suspend inline fun <reified C : YubiKeyConnection, T> YubiKeyDevice.withConnection(
     crossinline block: (C) -> T
 ): T = suspendCoroutine { continuation ->
     requestConnection(C::class.java) {
-        continuation.resumeWith(runCatching {
-            block(it.value)
-        })
+        runCatching { block(it.value) }
+            .onSuccess { result -> continuation.resume(result) }
+            .onFailure { exception -> continuation.resumeWithException(exception) }
     }
 }
