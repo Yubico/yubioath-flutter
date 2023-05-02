3723189cecc8339fba77dd1b995a245be9595e66
Dain Nilsson <dain@yubico.com>
2023-05-02 14:55:18 +0200

    Revamp OATH key actions and account dialogs.

--- lib/oath/views/key_actions.dart
+++ lib/oath/views/key_actions.dart
@@ -22,6 +22,7 @@ import 'package:yubico_authenticator/oath/icon_provider/icon_pack_dialog.dart';
 import '../../app/message.dart';
 import '../../app/models.dart';
 import '../../app/state.dart';
+import '../../app/views/fs_dialog.dart';
 import '../../core/state.dart';
 import '../../exception/cancellation_exception.dart';
 import '../../widgets/list_title.dart';
@@ -41,100 +42,115 @@ Widget oathBuildActions(
 }) {
   final l10n = AppLocalizations.of(context)!;
   final capacity = oathState.version.isAtLeast(4) ? 32 : null;
-  final theme = Theme.of(context).colorScheme;
-  return SimpleDialog(
-    children: [
-      ListTitle(l10n.s_setup, textStyle: Theme.of(context).textTheme.bodyLarge),
-      ListTile(
-        title: Text(l10n.s_add_account),
-        key: keys.addAccountAction,
-        leading:
-            const CircleAvatar(child: Icon(Icons.person_add_alt_1_outlined)),
-        subtitle: Text(used == null
-            ? l10n.l_unlock_first
-            : (capacity != null ? l10n.l_accounts_used(used, capacity) : '')),
-        enabled: used != null && (capacity == null || capacity > used),
-        onTap: used != null && (capacity == null || capacity > used)
-            ? () async {
-                final credentials = ref.read(credentialsProvider);
-                final withContext = ref.read(withContextProvider);
-                Navigator.of(context).pop();
-                CredentialData? otpauth;
-                if (isAndroid) {
-                  final scanner = ref.read(qrScannerProvider);
-                  if (scanner != null) {
-                    try {
-                      final url = await scanner.scanQr();
-                      if (url != null) {
-                        otpauth = CredentialData.fromUri(Uri.parse(url));
+  //final theme = Theme.of(context).colorScheme;
+  final theme =
+      ButtonTheme.of(context).colorScheme ?? Theme.of(context).colorScheme;
+  return FsDialog(
+    child: Column(
+      children: [
+        ListTitle(l10n.s_setup,
+            textStyle: Theme.of(context).textTheme.bodyLarge),
+        ListTile(
+          title: Text(l10n.s_add_account),
+          key: keys.addAccountAction,
+          leading: CircleAvatar(
+            backgroundColor: theme.primary,
+            foregroundColor: theme.onPrimary,
+            child: const Icon(Icons.person_add_alt_1_outlined),
+          ),
+          subtitle: Text(used == null
+              ? l10n.l_unlock_first
+              : (capacity != null ? l10n.l_accounts_used(used, capacity) : '')),
+          enabled: used != null && (capacity == null || capacity > used),
+          onTap: used != null && (capacity == null || capacity > used)
+              ? () async {
+                  final credentials = ref.read(credentialsProvider);
+                  final withContext = ref.read(withContextProvider);
+                  Navigator.of(context).pop();
+                  CredentialData? otpauth;
+                  if (isAndroid) {
+                    final scanner = ref.read(qrScannerProvider);
+                    if (scanner != null) {
+                      try {
+                        final url = await scanner.scanQr();
+                        if (url != null) {
+                          otpauth = CredentialData.fromUri(Uri.parse(url));
+                        }
+                      } on CancellationException catch (_) {
+                        // ignored - user cancelled
+                        return;
                       }
-                    } on CancellationException catch (_) {
-                      // ignored - user cancelled
-                      return;
                     }
                   }
+                  await withContext((context) async {
+                    await showBlurDialog(
+                      context: context,
+                      builder: (context) => OathAddAccountPage(
+                        devicePath,
+                        oathState,
+                        credentials: credentials,
+                        credentialData: otpauth,
+                      ),
+                    );
+                  });
                 }
-                await withContext((context) async {
-                  await showBlurDialog(
+              : null,
+        ),
+        ListTitle(l10n.s_manage,
+            textStyle: Theme.of(context).textTheme.bodyLarge),
+        ListTile(
+            key: keys.customIconsAction,
+            title: Text(l10n.s_custom_icons),
+            subtitle: Text(l10n.l_set_icons_for_accounts),
+            leading: CircleAvatar(
+              backgroundColor: theme.secondary,
+              foregroundColor: theme.onSecondary,
+              child: const Icon(Icons.image_outlined),
+            ),
+            onTap: () async {
+              Navigator.of(context).pop();
+              await ref.read(withContextProvider)((context) => showBlurDialog(
                     context: context,
-                    builder: (context) => OathAddAccountPage(
-                      devicePath,
-                      oathState,
-                      credentials: credentials,
-                      credentialData: otpauth,
-                    ),
-                  );
-                });
-              }
-            : null,
-      ),
-      ListTitle(l10n.s_manage,
-          textStyle: Theme.of(context).textTheme.bodyLarge),
-      ListTile(
-          key: keys.customIconsAction,
-          title: Text(l10n.s_custom_icons),
-          subtitle: Text(l10n.l_set_icons_for_accounts),
-          leading: const CircleAvatar(
-            child: Icon(Icons.image_outlined),
-          ),
-          onTap: () async {
-            Navigator.of(context).pop();
-            await ref.read(withContextProvider)((context) => showBlurDialog(
-                  context: context,
-                  routeSettings:
-                      const RouteSettings(name: 'oath_icon_pack_dialog'),
-                  builder: (context) => const IconPackDialog(),
-                ));
-          }),
-      ListTile(
-          key: keys.setOrManagePasswordAction,
-          title: Text(
-              oathState.hasKey ? l10n.s_manage_password : l10n.s_set_password),
-          subtitle: Text(l10n.l_optional_password_protection),
-          leading: const CircleAvatar(child: Icon(Icons.password_outlined)),
-          onTap: () {
-            Navigator.of(context).pop();
-            showBlurDialog(
-              context: context,
-              builder: (context) => ManagePasswordDialog(devicePath, oathState),
-            );
-          }),
-      ListTile(
-          key: keys.resetAction,
-          title: Text(l10n.s_reset_oath),
-          subtitle: Text(l10n.l_factory_reset_this_app),
-          leading: CircleAvatar(
-            foregroundColor: theme.onError,
-            backgroundColor: theme.error,
-            child: const Icon(Icons.delete_outline),
-          ),
-          onTap: () {
-            Navigator.of(context).pop();
-            showBlurDialog(
-              context: context,
-              builder: (context) => ResetDialog(devicePath),
-            );
-          }),
-    ],
+                    routeSettings:
+                        const RouteSettings(name: 'oath_icon_pack_dialog'),
+                    builder: (context) => const IconPackDialog(),
+                  ));
+            }),
+        ListTile(
+            key: keys.setOrManagePasswordAction,
+            title: Text(oathState.hasKey
+                ? l10n.s_manage_password
+                : l10n.s_set_password),
+            subtitle: Text(l10n.l_optional_password_protection),
+            leading: CircleAvatar(
+                backgroundColor: theme.secondary,
+                foregroundColor: theme.onSecondary,
+                child: const Icon(Icons.password_outlined)),
+            onTap: () {
+              Navigator.of(context).pop();
+              showBlurDialog(
+                context: context,
+                builder: (context) =>
+                    ManagePasswordDialog(devicePath, oathState),
+              );
+            }),
+        ListTile(
+            key: keys.resetAction,
+            title: Text(l10n.s_reset_oath),
+            subtitle: Text(l10n.l_factory_reset_this_app),
+            leading: CircleAvatar(
+              foregroundColor: theme.onError,
+              backgroundColor: theme.error,
+              child: const Icon(Icons.delete_outline),
+            ),
+            onTap: () {
+              Navigator.of(context).pop();
+              showBlurDialog(
+                context: context,
+                builder: (context) => ResetDialog(devicePath),
+              );
+            }),
+      ],
+    ),
   );
 }
