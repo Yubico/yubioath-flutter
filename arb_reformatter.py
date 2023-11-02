0b3d4518738c5637773200471144751e50615041
Emil Lundberg <emil@yubico.com>
2023-11-02 13:40:16 +0100

    Use JSON parser in arb_reformatter.py

--- arb_reformatter.py
+++ arb_reformatter.py
@@ -1,107 +1,72 @@
 #!/usr/bin/env python3
 
+import json
 import os
+
 
 # Function to load the contents of a file into a line list
 def read_file_lines(file_path):
     with open(file_path, 'r', encoding='utf-8') as file:
         return file.readlines()
 
+
+# Function to load the contents of a file into a line list
+def read_file_json(file_path):
+    with open(file_path, 'r', encoding='utf-8') as file:
+        return json.load(file)
+
+
 # Function to save a list of lines to a file
-def write_lines_to_file(file_path, lines):
+def write_to_file(file_path, text):
     with open(file_path, 'w', encoding='utf-8') as file:
-        file.writelines(lines)
+        file.write(text)
+
+
+# Move keys in target into same order as in source.
+# Keys not present in source are removed from target.
+# Returns a new value of target; does not mutate arguments.
+def equalize_key_order(source, target):
+    if isinstance(source, dict) and isinstance(target, dict):
+        target_result = {}
+        for key in source.keys():
+            if key in target and target[key] is not None:
+                source_value = source[key]
+                target_value = target[key]
+                target_result[key] = equalize_key_order(source_value, target_value)
+
+            else:
+                if key.startswith("@"):
+                    target_result[key] = source[key]
+                else:
+                    target_result[key] = None
+        return target_result
+
+    elif isinstance(source, list) and isinstance(target, list):
+        return [
+            equalize_key_order(source_item, target_item)
+            for source_item, target_item in zip(source, target, strict=True)
+        ]
+
+    else:
+        return target
+
 
 # Function to update the ARB files
 def update_arb_file(source_path, target_path, language_code):
-    # Load the contents of the source file 'app_en.arb'
     source_lines = read_file_lines(source_path)
+    source_json = read_file_json(source_path)
+    target_json = read_file_json(target_path)
 
-    # Load the contents of the target file 'app_**.arb'
-    target_lines = read_file_lines(target_path)
+    target_reordered = equalize_key_order(source_json, target_json)
+    target_text = json.dumps(target_reordered, indent=4, ensure_ascii=False)
+    target_lines = target_text.splitlines()
 
-    # Create a translation dictionary based on the contents of the target file
-    translation_dict = {}
-    in_placeholders = False
-    in_readme = False
-    in_lint_rules = False
+    for i, line in enumerate(source_lines):
+        if line.strip() == "":
+            target_lines.insert(i, "")
 
-    for line in target_lines:
-        if line.strip() == '"placeholders": {':
-            in_placeholders = True
-            continue
-        elif line.strip() == '"@_readme": {':
-            in_readme = True
-            continue
-        elif line.strip() == '"@_lint_rules": {':
-            in_lint_rules = True
-            continue
+    write_to_file(target_path, "\n".join(target_lines))
 
-        if in_placeholders:
-            if line.strip() == '},':
-                in_placeholders = False
-            continue
-        elif in_readme:
-            if line.strip() == '},':
-                in_readme = False
-            continue
-        elif in_lint_rules:
-            if line.strip() == '},':
-                in_lint_rules = False
-            continue
-
-        if ':' in line:
-            key, value = line.split(':', 1)
-            key = key.strip().strip('"')
-            value = value.strip().strip(",").strip('"')
-            if not key.startswith("@"):
-                # only add non special keys
-                translation_dict[key] = value
-
-    # Update the target file based on the source file
-    updated_target_lines = []
-
-    for line in source_lines:
-        if line.strip() == '"placeholders": {':
-            in_placeholders = True
-        elif line.strip() == '"@_readme": {':
-            in_readme = True
-        elif line.strip() == '"@_lint_rules": {':
-            in_lint_rules = True
-
-        if in_placeholders:
-            if line.strip() == '},':
-                in_placeholders = False
-        elif in_readme:
-            if line.strip() == '},':
-                in_readme = False
-        elif in_lint_rules:
-            if line.strip() == '},':
-                in_lint_rules = False
-
-        if '"@@locale": "en"' in line:
-            line = line.replace('"@@locale": "en"', f'"@@locale": "{language_code}"')
-
-        if ':' in line:
-            key, value = line.split(':', 1)
-            key = key.strip().strip('"')
-            if key in translation_dict:
-                updated_line = f'    "{key}": "{translation_dict[key]}",\n'
-                updated_target_lines.append(updated_line)
-            elif key.startswith("@_eof"):
-                # eof will be the last line, will not have trailing comma
-                updated_target_lines.append(line.strip(","))
-            elif in_readme or in_lint_rules or in_placeholders or key.startswith("@"):
-                # replicate lines from special sections
-                updated_target_lines.append(line)
-            else:
-                # if a key is missing, don't include it at all
-                updated_target_lines.append('\n')
-        else:
-            updated_target_lines.append(line)
-
-    # Save the updated target file
-    write_lines_to_file(target_path, updated_target_lines)
 
 if __name__ == "__main__":
     source_file_path = 'lib/l10n/app_en.arb'
