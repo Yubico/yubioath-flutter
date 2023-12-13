8fd753431d1bb741a0466de7d20e91d4a84476ec
Elias Bonnici <elias.bonnici@yubico.com>
2023-12-13 19:36:50 +0100

    Don't close FIDO reset dialog on done.

--- lib/fido/views/reset_dialog.dart
+++ lib/fido/views/reset_dialog.dart
@@ -43,71 +43,76 @@ class ResetDialog extends ConsumerStatefulWidget {
 class _ResetDialogState extends ConsumerState<ResetDialog> {
   StreamSubscription<InteractionEvent>? _subscription;
   InteractionEvent? _interaction;
-  int _currentStep = 0;
-  final _totalSteps = 4;
+  int _currentStep = -1;
+  final _totalSteps = 3;
 
   String _getMessage() {
     final l10n = AppLocalizations.of(context)!;
     final nfc = widget.node.transport == Transport.nfc;
+    if (_currentStep == 3) {
+      return l10n.l_fido_app_reset;
+    }
     return switch (_interaction) {
       InteractionEvent.remove =>
         nfc ? l10n.l_remove_yk_from_reader : l10n.l_unplug_yk,
       InteractionEvent.insert =>
         nfc ? l10n.l_replace_yk_on_reader : l10n.l_reinsert_yk,
       InteractionEvent.touch => l10n.l_touch_button_now,
-      null => l10n.l_press_reset_to_begin
+      null => ''
     };
   }
 
   @override
   Widget build(BuildContext context) {
     final l10n = AppLocalizations.of(context)!;
-    double progress = _currentStep == 0 ? 0.0 : _currentStep / (_totalSteps);
+    double progress = _currentStep == -1 ? 0.0 : _currentStep / (_totalSteps);
     return ResponsiveDialog(
       title: Text(l10n.s_factory_reset),
-      onCancel: () {
-        _subscription?.cancel();
-      },
-      actions: [
-        TextButton(
-          onPressed: _subscription == null
-              ? () async {
-                  _subscription = ref
-                      .read(fidoStateProvider(widget.node.path).notifier)
-                      .reset()
-                      .listen((event) {
-                    setState(() {
-                      _currentStep++;
-                      _interaction = event;
-                    });
-                  }, onDone: () {
-                    setState(() {
-                      _currentStep++;
-                    });
-                    _subscription = null;
-                    Navigator.of(context).pop();
-                    showMessage(context, l10n.l_fido_app_reset);
-                  }, onError: (e) {
-                    _log.error('Error performing FIDO reset', e);
-                    Navigator.of(context).pop();
-                    final String errorMessage;
-                    // TODO: Make this cleaner than importing desktop specific RpcError.
-                    if (e is RpcError) {
-                      errorMessage = e.message;
-                    } else {
-                      errorMessage = e.toString();
-                    }
-                    showMessage(
-                      context,
-                      l10n.l_reset_failed(errorMessage),
-                      duration: const Duration(seconds: 4),
-                    );
-                  });
-                }
-              : null,
-          child: Text(l10n.s_reset),
-        ),
-      ],
+      onCancel: _currentStep < 3
+          ? () {
+              _subscription?.cancel();
+            }
+          : null,
+      actions: _currentStep < 3
+          ? [
+              TextButton(
+                onPressed: _subscription == null
+                    ? () async {
+                        _subscription = ref
+                            .read(fidoStateProvider(widget.node.path).notifier)
+                            .reset()
+                            .listen((event) {
+                          setState(() {
+                            _currentStep++;
+                            _interaction = event;
+                          });
+                        }, onDone: () {
+                          setState(() {
+                            _currentStep++;
+                          });
+                          _subscription = null;
+                        }, onError: (e) {
+                          _log.error('Error performing FIDO reset', e);
+                          Navigator.of(context).pop();
+                          final String errorMessage;
+                          // TODO: Make this cleaner than importing desktop specific RpcError.
+                          if (e is RpcError) {
+                            errorMessage = e.message;
+                          } else {
+                            errorMessage = e.toString();
+                          }
+                          showMessage(
+                            context,
+                            l10n.l_reset_failed(errorMessage),
+                            duration: const Duration(seconds: 4),
+                          );
+                        });
+                      }
+                    : null,
+                child: Text(l10n.s_reset),
+              ),
+            ]
+          : [],
       child: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 18.0),
         child: Column(
@@ -120,8 +125,10 @@ class _ResetDialogState extends ConsumerState<ResetDialog> {
             Text(
               l10n.p_warning_disable_accounts,
             ),
-            Text('Status: ${_getMessage()}'),
-            LinearProgressIndicator(value: progress),
+            if (_currentStep > -1) ...[
+              Text('Status: ${_getMessage()}'),
+              LinearProgressIndicator(value: progress)
+            ],
           ]
               .map((e) => Padding(
                     padding: const EdgeInsets.symmetric(vertical: 8.0),
