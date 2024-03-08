277a7bcab756228e1bf577e757866d92e8abd608
Dain Nilsson <dain@yubico.com>
2024-03-08 14:06:13 +0100

    Remove custom NFC icon

--- lib/widgets/custom_icons.dart
+++ lib/widgets/custom_icons.dart
@@ -14,8 +14,6 @@
  * limitations under the License.
  */
 
-import 'dart:math';
-
 import 'package:flutter/material.dart';
 import 'package:material_symbols_icons/symbols.dart';
 
@@ -61,42 +59,6 @@ class _StrikethroughPainter extends CustomPainter {
 
     canvas.drawLine(Offset(size.width * 0.15, size.height * 0.15),
         Offset(size.width * 0.8, size.height * 0.8), paint);
-  }
-
-  @override
-  bool shouldRepaint(covariant CustomPainter oldDelegate) {
-    return false;
-  }
-}
-
-final Widget nfcIcon = Builder(builder: (context) {
-  final theme = IconTheme.of(context);
-  return CustomPaint(
-    size: Size.square(theme.size ?? 24),
-    painter: _NfcIconPainter(theme.color ?? Colors.black),
-  );
-});
-
-class _NfcIconPainter extends CustomPainter {
-  final Color color;
-  _NfcIconPainter(this.color);
-
-  @override
-  void paint(Canvas canvas, Size size) {
-    final step = size.width / 4;
-    const sweep = pi / 4;
-
-    final paint = Paint()
-      ..color = color
-      ..style = PaintingStyle.stroke
-      ..strokeCap = StrokeCap.round
-      ..strokeWidth = step / 2;
-
-    final rect =
-        Offset(size.width * -1.7, 0) & Size(size.width * 2, size.height);
-    for (var i = 0; i < 3; i++) {
-      canvas.drawArc(rect.inflate(i * step), -sweep / 2, sweep, false, paint);
-    }
   }
 
   @override
