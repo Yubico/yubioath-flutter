5ff3a15b1220dece22d20784aaa76b9b31c35733
Elias Bonnici <elias.bonnici@yubico.com>
2024-04-16 16:01:38 +0200

    Change default window size

--- lib/desktop/window_manager_helper/defaults.dart
+++ lib/desktop/window_manager_helper/defaults.dart
@@ -18,6 +18,7 @@ import 'dart:ui';
 
 class WindowDefaults {
   static const minWidth = 300.0;
-  static const minSize = Size(minWidth, 0.0);
-  static const bounds = Rect.fromLTWH(10.0, 10.0, 400.0, 720.0);
+  static const minHeight = 375.0;
+  static const minSize = Size(minWidth, minHeight);
+  static const bounds = Rect.fromLTWH(10.0, 10.0, 1100.0, 700.0);
 }
