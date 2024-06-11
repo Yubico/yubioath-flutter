a34e02506b18d37393e0edb938c9bb6912ca09bb
Adam Velebil <adam.velebil@yubico.com>
2024-06-11 14:05:31 +0200

    add tap actions

--- android/app/src/main/kotlin/com/yubico/authenticator/widget/AppWidget.kt
+++ android/app/src/main/kotlin/com/yubico/authenticator/widget/AppWidget.kt
@@ -16,13 +16,19 @@
 
 package com.yubico.authenticator.widget
 
+import android.app.PendingIntent
 import android.appwidget.AppWidgetManager
 import android.appwidget.AppWidgetProvider
 import android.content.Context
 import android.content.Intent
+import android.os.Build
 import android.view.View
 import android.widget.RemoteViews
+import android.widget.Toast
+import com.yubico.authenticator.ClipboardUtil
+import com.yubico.authenticator.MainActivity
 import com.yubico.authenticator.R
+import com.yubico.authenticator.compatUtil
 
 class AppWidget : AppWidgetProvider() {
 
@@ -31,6 +37,10 @@ class AppWidget : AppWidgetProvider() {
         var latestCode: String = ""
         var latestIssuer: String = ""
         var latestAccountName: String = ""
+
+        const val ACTION_OPEN_APP = "OPEN_APP"
+        const val ACTION_COPY_CODE = "COPY_CODE"
+        const val EXTRA_CODE = "CODE"
     }
 
     override fun onUpdate(
@@ -56,7 +66,32 @@ class AppWidget : AppWidgetProvider() {
     }
 
     override fun onReceive(context: Context?, intent: Intent?) {
-        super.onReceive(context, intent)
+        when (intent?.action) {
+            ACTION_OPEN_APP -> {
+                val mainAppIntent = Intent(context, MainActivity::class.java).apply {
+                    flags = Intent.FLAG_ACTIVITY_NEW_TASK
+                }
+                context?.startActivity(mainAppIntent)
+            }
+
+            ACTION_COPY_CODE -> {
+
+                context?.let {
+                    val code = intent.extras?.getString(EXTRA_CODE)
+                    code?.let {
+                        ClipboardUtil.setPrimaryClip(context, code, false)
+                        compatUtil.until(Build.VERSION_CODES.TIRAMISU) {
+                            Toast.makeText(context, "Copied code to clipboard", Toast.LENGTH_LONG)
+                                .show()
+                        }
+                    }
+                }
+
+            }
+
+            else -> super.onReceive(context, intent)
+        }
+
 
     }
 }
@@ -74,13 +109,32 @@ internal fun updateAppWidget(
         views.setTextViewText(R.id.appwidget_text, renderCode)
         views.setTextViewText(R.id.issuer_text, AppWidget.latestIssuer)
         views.setTextViewText(R.id.account_text, AppWidget.latestAccountName)
+
+        // open app
+        val copyCodeIntent = Intent(context, AppWidget::class.java).apply {
+            action = AppWidget.ACTION_COPY_CODE
+            putExtra(AppWidget.EXTRA_CODE, AppWidget.latestCode)
+        }
+        val copyCodePendingIntent =
+            PendingIntent.getBroadcast(context, 0, copyCodeIntent, PendingIntent.FLAG_IMMUTABLE)
+        views.setOnClickPendingIntent(R.id.appwidget_copy_btn, copyCodePendingIntent)
+
     } else {
         views.setViewVisibility(R.id.appwidget_tap_key_layout, View.VISIBLE)
         views.setViewVisibility(R.id.appwidget_code_layout, View.GONE)
         views.setTextViewText(R.id.appwidget_text, "")
         views.setTextViewText(R.id.issuer_text, "")
         views.setTextViewText(R.id.account_text, "")
-
     }
+
+    // open app
+    val openAppIntent = Intent(context, AppWidget::class.java).apply {
+        action = AppWidget.ACTION_OPEN_APP
+    }
+    val openAppPendingIntent =
+        PendingIntent.getBroadcast(context, 0, openAppIntent, PendingIntent.FLAG_IMMUTABLE)
+    views.setOnClickPendingIntent(R.id.appwidget, openAppPendingIntent)
+
     appWidgetManager.updateAppWidget(appWidgetId, views)
 }
+
