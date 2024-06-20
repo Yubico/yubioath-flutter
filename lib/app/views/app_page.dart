c1e53a7611d28e2f09a3035da6a411e5aa79cf46
Elias Bonnici <elias.bonnici@yubico.com>
2024-06-20 13:11:46 +0200

    Change position of toggle sidebar icon

--- lib/app/views/app_page.dart
+++ lib/app/views/app_page.dart
@@ -669,26 +669,6 @@ class _AppPageState extends ConsumerState<AppPage> {
                 )
               : null,
           actions: [
-            if (hasManage &&
-                (widget.keyActionsBuilder != null ||
-                    widget.detailViewBuilder != null))
-              Padding(
-                padding: const EdgeInsets.only(left: 4),
-                child: IconButton(
-                  key: toggleDetailViewIconButtonKey,
-                  onPressed: () {
-                    ref
-                        .read(_detailViewVisibilityProvider.notifier)
-                        .toggleExpanded();
-                  },
-                  icon: const Icon(Symbols.view_sidebar),
-                  iconSize: 24,
-                  tooltip: showDetailView
-                      ? l10n.s_collapse_sidebar
-                      : l10n.s_expand_sidebar,
-                  padding: const EdgeInsets.all(12),
-                ),
-              ),
             if (widget.actionButtonBuilder == null &&
                 (widget.keyActionsBuilder != null &&
                     (!hasManage || !showDetailView)))
@@ -715,6 +695,26 @@ class _AppPageState extends ConsumerState<AppPage> {
                       : const Icon(Symbols.more_vert),
                   iconSize: 24,
                   tooltip: l10n.s_configure_yk,
+                  padding: const EdgeInsets.all(12),
+                ),
+              ),
+            if (hasManage &&
+                (widget.keyActionsBuilder != null ||
+                    widget.detailViewBuilder != null))
+              Padding(
+                padding: const EdgeInsets.only(left: 4),
+                child: IconButton(
+                  key: toggleDetailViewIconButtonKey,
+                  onPressed: () {
+                    ref
+                        .read(_detailViewVisibilityProvider.notifier)
+                        .toggleExpanded();
+                  },
+                  icon: const Icon(Symbols.view_sidebar),
+                  iconSize: 24,
+                  tooltip: showDetailView
+                      ? l10n.s_collapse_sidebar
+                      : l10n.s_expand_sidebar,
                   padding: const EdgeInsets.all(12),
                 ),
               ),
