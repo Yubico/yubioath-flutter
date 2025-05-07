244b37d7dcf87ef37cea8dccac86c55f2d62e407
Elias Bonnici <elias.bonnici@yubico.com>
2025-05-07 14:33:09 +0200

    Add shell script for running pull-translations

---
+++ run-pull-translations.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+env_name="crowdin_env"
+# Create venv if it does not exist
+if [ ! -d "$env_name" ]; then
+    echo "Creating virtual env '$env_name'"
+    python3 -m venv "$env_name"
+else
+    echo "Using already existing environment '$env_name'"
+fi
+
+# Activate venv
+source "./$env_name/bin/activate"
+
+# Install dependencies
+echo "Installing dependencies..."
+pip install pyaml
+
+# Run script
+echo "Running pull-translations.py..."
+python3 pull-translations.py
+
+# Deactivate venv
+deactivate
