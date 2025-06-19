ce6c7634a267731b9d79af8e415e02f4e10fdbec
Dain Nilsson <dain@yubico.com>
2025-06-19 14:20:35 +0200

    Replace poetry with uv

--- build-helper.bat
+++ build-helper.bat
@@ -2,14 +2,14 @@
 
 echo Building authenticator-helper for Windows...
 cd helper
-poetry install || goto :error
+uv sync --locked || goto :error
 rmdir /s /q ..\build\windows\helper
-poetry run pyinstaller authenticator-helper.spec --distpath ..\build\windows || goto :error
+uv run pyinstaller authenticator-helper.spec --distpath ..\build\windows || goto :error
 
 echo Generating license files...
 rmdir /s /q ..\build\windows\helper-license-venv
-poetry build || goto :error
-poetry run python -m venv ..\build\windows\helper-license-venv || goto :error
+uv build || goto :error
+uv run python -m venv ..\build\windows\helper-license-venv || goto :error
 ..\build\windows\helper-license-venv\Scripts\python -m pip install --upgrade pip wheel || goto :error
 ..\build\windows\helper-license-venv\Scripts\python -m pip install dist\authenticator_helper-0.1.0-py3-none-any.whl pip-licenses || goto :error
 ..\build\windows\helper-license-venv\Scripts\pip-licenses --format=json --no-license-path --with-license-file --ignore-packages authenticator-helper zxing-cpp --output-file ..\assets\licenses\helper.json || goto :error
