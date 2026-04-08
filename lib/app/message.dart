df3442ebda35d81929b8638e6ae9d4e1c0e1590d
Adam Velebil <adam.velebil@yubico.com>
2026-04-08 14:43:49 +0200

    fix: set explicitChildNodes to true
    Flutter's semantics framework requires that when scopesRoute is true, explicitChildNodes must also be set to true.

--- lib/app/message.dart
+++ lib/app/message.dart
@@ -56,7 +56,7 @@ Future<T?> showBlurDialog<T>({
   barrierColor: barrierColor,
   barrierLabel: MaterialLocalizations.of(context).modalBarrierDismissLabel,
   pageBuilder: (ctx, anim1, anim2) =>
-      Semantics(scopesRoute: true, namesRoute: true, child: builder(ctx)),
+      Semantics(scopesRoute: true, explicitChildNodes: true, namesRoute: true, child: builder(ctx)),
   transitionDuration: const Duration(milliseconds: 150),
   transitionBuilder: (ctx, anim1, anim2, child) => BackdropFilter(
     filter: ImageFilter.blur(
