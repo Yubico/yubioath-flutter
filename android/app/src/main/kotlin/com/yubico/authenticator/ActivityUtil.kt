3d5e3805ee569b2f2625f1791739953e24d5e217
Adam Velebil <adam.velebil@yubico.com>
2026-05-07 09:28:58 +0200

    fix(android): split NDEF and App Link aliases
    - Split AliasNdefActivity so NDEF_DISCOVERED and the
      ACTION_VIEW + autoVerify App Link filter live on
      separate activity-aliases targeting NdefActivity
    - Prevents App Link verification state from interfering
      with NFC dispatch, which was falling through to the
      browser
    - Toggle both aliases together in enable/disable
      AppNfcDiscovery so the NFC pref applies on both the
      Android 16+ App Link path and the legacy NDEF path
    - Keep COMPONENT_ENABLED_STATE_DISABLED for the off
      path; DEFAULT would leave aliases visible in the
      system NFC chooser

--- android/app/src/main/kotlin/com/yubico/authenticator/ActivityUtil.kt
+++ android/app/src/main/kotlin/com/yubico/authenticator/ActivityUtil.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2023 Yubico.
+ * Copyright (C) 2023-2026 Yubico.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -62,36 +62,42 @@ class ActivityUtil(private val activity: Activity) {
     /**
      * The system will start the app when an NDEF tag with recognized URI is discovered.
      *
-     * Calling this method will enable <code>AliasNdefActivity</code> alias and the intent-filter
-     * for android.nfc.action.NDEF_DISCOVERED will be active. This is the default behavior as
-     * defined in the AndroidManifest.xml.
+     * Calling this method will enable both <code>AliasNdefActivity</code> (handling
+     * android.nfc.action.NDEF_DISCOVERED on Android 15 and below, and as a fallback on builds
+     * whose App Links cannot be verified) and <code>AliasNdefAppLinkActivity</code> (handling
+     * the Android 16+ android.intent.action.VIEW dispatch for verified App Links). This is the
+     * default behavior as defined in the AndroidManifest.xml.
      *
-     * For list of discoverable URIs see the alias definition in AndroidManifest.xml.
+     * For list of discoverable URIs see the alias definitions in AndroidManifest.xml.
      *
      * @see <a href="https://developer.android.com/guide/topics/manifest/activity-alias-element">Activity Alias in Android SDK documentation</a>
      */
     fun enableAppNfcDiscovery() {
-        setState(
-            NDEF_ACTIVITY_ALIAS,
-            PackageManager.COMPONENT_ENABLED_STATE_DEFAULT
+        setState(NDEF_ACTIVITY_ALIAS, PackageManager.COMPONENT_ENABLED_STATE_DEFAULT)
+        setState(NDEF_APP_LINK_ACTIVITY_ALIAS, PackageManager.COMPONENT_ENABLED_STATE_DEFAULT)
+        logger.debug(
+            "Enabled NFC discovery by setting state of $NDEF_ACTIVITY_ALIAS and $NDEF_APP_LINK_ACTIVITY_ALIAS to DEFAULT"
         )
-        logger.debug("Enabled NFC discovery by setting state of $NDEF_ACTIVITY_ALIAS to DEFAULT")
     }
 
     /**
      * The system will ignore the app when NDEF tags are discovered.
      *
-     * Calling this method will disable <code>AliasNdefActivity</code> alias and there will be no
-     * active intent-filter for android.nfc.action.NDEF_DISCOVERED.
+     * Calling this method will disable both <code>AliasNdefActivity</code> and
+     * <code>AliasNdefAppLinkActivity</code>, removing them from the system NFC resolver so the
+     * app is neither offered as a handler nor launched on tap. <code>DISABLED</code> (rather
+     * than <code>DEFAULT</code>) is required because the manifest declares both aliases as
+     * <code>android:enabled="true"</code>; <code>DEFAULT</code> would leave them visible in the
+     * NFC chooser.
      *
      * @see <a href="https://developer.android.com/guide/topics/manifest/activity-alias-element">Activity Alias in Android SDK documentation</a>
      */
     fun disableAppNfcDiscovery() {
-        setState(
-            NDEF_ACTIVITY_ALIAS,
-            PackageManager.COMPONENT_ENABLED_STATE_DISABLED
+        setState(NDEF_ACTIVITY_ALIAS, PackageManager.COMPONENT_ENABLED_STATE_DISABLED)
+        setState(NDEF_APP_LINK_ACTIVITY_ALIAS, PackageManager.COMPONENT_ENABLED_STATE_DISABLED)
+        logger.debug(
+            "Disabled NFC discovery by setting state of $NDEF_ACTIVITY_ALIAS and $NDEF_APP_LINK_ACTIVITY_ALIAS to DISABLED"
         )
-        logger.debug("Disabled NFC discovery by setting state of $NDEF_ACTIVITY_ALIAS to DISABLED")
     }
 
     private fun setState(aliasName: String, enabledState: Int) {
@@ -107,6 +113,7 @@ class ActivityUtil(private val activity: Activity) {
 
     companion object {
         const val NDEF_ACTIVITY_ALIAS = "AliasNdefActivity"
+        const val NDEF_APP_LINK_ACTIVITY_ALIAS = "AliasNdefAppLinkActivity"
         const val MAIN_ACTIVITY_ALIAS = "AliasMainActivity"
     }
 }
